d7e1b57dbf6941c86b59c04de2323e1478589f12
roman <you@example.com>
2023-06-21 16:05:46 +0300

    switchcase

--- switchcase/Program.cs
+++ switchcase/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace switchcase {
+    
+    
+    class Program {
+
+        static void Main() {
+
+            short user_input = Convert.ToInt16(Console.ReadLine());
+            switch(user_input) {
+                case 5:
+                    Console.WriteLine("Number is 5");
+                    break;
+                case 15:
+                    Console.WriteLine("Number is 15");
+                    break;
+                case 20:
+                    Console.WriteLine("Number is 20");
+                    break;
+                case 115:
+                    Console.WriteLine("Number is 115");
+                    break;
+                default:
+                    Console.WriteLine("Number is not detection");
+                    break;
+            }
+        }
+    }
+}
