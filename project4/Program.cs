76d6875770f683c6496aa45f4e44a6c7c95a80c4
roman <you@example.com>
2023-06-21 16:39:55 +0300

    project4 end

--- project4/Program.cs
+++ project4/Program.cs
@@ -1,1 +1,31 @@
-﻿
+﻿using System;
+
+namespace project4 {
+
+    class Program {
+
+        static void Main() {
+        
+            // for(float i = 240; i > 10; i /=2) {
+                // Console.WriteLine("Element: {0}", i);
+
+            // byte i = 10;    
+            // while (i >= 1) {
+                // Console.WriteLine(i);
+                // i--;
+
+            // bool isHasCar = true;
+            // while(isHasCar) {
+                // string end = Console.ReadLine();
+                // if(end == "end") 
+                    // isHasCar = false;
+
+               byte i = 100;
+
+                do {
+                    Console.WriteLine(i);
+                } while(i < 10);
+            }
+        }
+    }
+
