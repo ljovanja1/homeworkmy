dce53f1c7ff34526c3881a3f7ec6673ddb24833d
roman <you@example.com>
2023-06-21 15:43:55 +0300

    projec32

--- project3/Program.cs
+++ project3/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace project2 {
+
+    class Program {
+
+        static void Main() {
+         int num_1 = 0, num_2 = 0;
+
+         num_1 = Console.ToInt32(Console.ReadLine());
+         num_2 = Console.ToInt32(Console.ReadLine());
+            // Получение данных
+            Console.WriteLine("First " + num_1 + "second" + num_2);
+        }
+
+    }
+
+}
