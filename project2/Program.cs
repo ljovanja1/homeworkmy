1b8adc6a97ac67600979827b9378670489fc25ff
roman <you@example.com>
2023-06-21 15:37:15 +0300

    project2

--- project2/Program.cs
+++ project2/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace project2 {
+
+    class Program {
+
+        static void Main() {
+            uint number = 10;
+            // int - ЛЮБОЕ ЧИСЛО ОТРИЦАТЕЛЬНОЕ И ПОЛОЖИТЕЛЬНОЕ
+            // uint - ТОЛЬКО ПОЛОЖИТЕЛЬНЫЕ
+            // byte = 255
+            // short = 3200;
+            // int = 2млрд
+            // long = 10трилиардов.
+            float num = 1425463.6568f;
+            double num2 = 0.54367587698d;
+            number = 10;
+            Console.WriteLine(number);
+            number = number + 10;
+            Console.WriteLine("Переменная: " + num2 + ".");
+
+            // Получение данных
+            Console.ReadKey();
+        }
+
+    }
+
+}
