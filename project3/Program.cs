39c23462176cfb0fd59bb86e5820aa497ca8d1dd
roman <you@example.com>
2023-06-21 15:53:27 +0300

    projec32

--- project3/Program.cs
+++ project3/Program.cs
@@ -5,10 +5,12 @@ namespace project2 {
     class Program {
 
         static void Main() {
-         int num_1 = 1, num_2 = 10;
+        
+            uint num_1 = 0;
+            int num_2 = 0;
 
-         num_1 = Convert.ToInt32(Console.ReadLine());
-         num_2 = Convert.ToInt32(Console.ReadLine());
+            num_1 = Convert.ToInt32(Console.ReadLine());
+            num_2 = Convert.ToInt32(Console.ReadLine());
             // Получение данных
             Console.WriteLine("First " + num_1 + "second" + num_2);
         }
