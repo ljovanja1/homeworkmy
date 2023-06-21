b4b6dd0fd2dc82196b3261f4f95b44e009368dc1
roman <you@example.com>
2023-06-21 15:14:06 +0300

    add project

--- project/Program.cs
+++ project/Program.cs
@@ -7,7 +7,19 @@ namespace project {
         static void Main () 
         {
             Console.WriteLine ("hello");
-            Console.ReadKey();
+            Console.Write ("hello2\n\n");
+            // Коментарий любой для себя \n - WriteLine
+            /*
+                Огромный 
+                комент
+
+                мывмылво
+                бесконечный
+
+            */
+            
+            Console.WriteLine ("hello3");
+            Console.ReadLine();
         }
 
     }
